bf7703a9174c613f64b8700005b7cfe5d5375a69
gchatain <gchatain@student.42lyon.fr>
2023-05-17 09:43:39 +0200

    create item route

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from '../authentification/jwt.guard';
 import { ItemService } from './item.service';
 
@@ -8,8 +8,23 @@ export class ItemController {
   constructor(readonly itemService: ItemService) {}
 
   @Get()
-  public async getItems() {
-    return await this.itemService.getItems();
+  public async getItems(@Res() response){
+    return response.status(200).send(await this.itemService.getItems());
+  }
+
+  @Post()
+  public async createItem(@Res() response, @Body() body){
+    let ret;
+    try {
+      ret = await this.itemService.createItem(
+        body.name,
+        body.description,
+        body.price,
+      );
+    } catch (e) {
+      return response.status(400).send(e.message);
+    }
+    return response.status(201).send(ret);
   }
 
   @Get('id/:id')
