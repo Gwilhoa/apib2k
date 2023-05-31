6c5b4daedfabced6b5355e5ebbe42b60f690c127
gchatain <gchatain@student.42lyon.fr>
2023-05-31 21:47:21 +0200

    oups.jwt

--- src/authentification/auth.decorator.ts
+++ src/authentification/auth.decorator.ts
@@ -1,8 +1,7 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.username;
+    return ctx.switchToHttp().getRequest();
   },
 );
