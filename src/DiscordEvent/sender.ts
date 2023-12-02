cddb909f2eadc5f514ae290b15f25b2df0e25b01
gchatain <gchatain@student.42lyon.fr>
2023-12-02 18:12:10 +0100

    fix prblm envois

--- src/DiscordEvent/sender.ts
+++ src/DiscordEvent/sender.ts
@@ -8,6 +8,10 @@ export async function sendMessageToUser(user, message) {
 
 export async function getAvatarUrl(user) {
   const guild = await client.guilds.fetch('382938797442334720');
-  const member = await guild.members.fetch(user.id);
-  return member.user.avatarURL();
+  try {
+    const member = await guild.members.fetch(user.id);
+    return member.user.avatarURL();
+  } catch (e) {
+    return null;
+  }
 }
